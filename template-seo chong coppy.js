3e17f9694d343f29487d7c07dced06d90dba269d
uyentoda <45456811+uyentoda@users.noreply.github.com>
2018-12-04 10:18:38 +0700

    Add files via upload

--- template-seo chong coppy.js
+++ template-seo chong coppy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://duan-everdecity.blogspot.com//'>https://duan-everdecity.blogspot.com/"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://duan-everdecity.blogspot.com/'>https://duan-everdecity.blogspot.com/"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
@@ -15,3 +15,6 @@ function FSAddOriginalLink(){
     },0);
 }
 document.oncopy = FSAddOriginalLink;
+
+
+MUON COPY THI VAO DAY : http://linkingland.com.vn/everde-city//
